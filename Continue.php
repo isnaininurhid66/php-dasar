e85b04a7c797741049f0f28220961f1fb0a4535f
isnaininurhid66 <isnaininurhidayat@gmail.com>
2021-11-09 23:08:11 +0700

    add if

--- Continue.php
+++ Continue.php
@@ -1,3 +1,10 @@
 <?php
 
-for ($counter =0; ) 
+$counter= 1;
+
+for ($counter = 0; $counter <= 100; $counter++) {
+if ( $counter % 2 == 0) {
+    $continue;
+}
+ echo "Hello Continue" . $counter. PHP_EOL;
+} 
